b664ca5e9df76687467ca6b7bd3d929ab164baea
ChrisM345 <chrism.12277@gmail.com>
2024-03-16 11:58:03 -0500

    Created dialog element in order to get inputs for new todo item

--- src/index.js
+++ src/index.js
@@ -7,7 +7,29 @@ navigationBar()
 getProjectList()
 addProject('test')
 
+const dialog = document.querySelector('dialog');
+const showButton = document.querySelector('dialog + button');
+const closeButton = document.querySelector('.close');
+const btnSubmit = document.querySelector(".btnSubmit");
+
 const todoButton = document.querySelector('.todo-button')
 todoButton.addEventListener('click', () => {
-    addTask('newTask')
+    dialog.showModal();// addTask('newTask')
+})
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+})
+
+btnSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title').value;
+    const description = document.querySelector('#description').value;
+    const dueDate = document.querySelector('#due-date').value;
+    const priority = document.querySelector('input[name="priority"]:checked').value;
+    if (title && description && dueDate && priority){
+        addTask(title, description, dueDate, priority);
+        dialog.firstChild.reset();
+        dialog.close();
+    }
 })
--- src/todo-item.js
+++ src/todo-item.js
@@ -1,6 +1,35 @@
 import { addProject } from "./navigation"
 
-export default function addTask(task) {
+const listOfTodos = [];
+
+export default function addTask(title, description, dueDate, priority) {
     console.log("in the function")
-    addProject(task)
+    // addProject(task)
+    // let task = new TodoItem
+    console.log(title)
+    console.log(description)
+    console.log(dueDate)
+    console.log(priority)
 }
+
+
+class TodoItem {
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    addItem(){
+        listOfTodos.push(this)
+    }
+
+    listItems(){
+        console.log(listOfTodos)
+    }
+}
+
+// Brainstorm what kind of properties your todo-items are going to have. 
+// At a minimum they should have a title, description, dueDate and priority. 
+// You might also want to include notes or even a checklist.
